5768c557bd5c6924a8a36b7f8b86a192bffefe2a
Manuel <Manuel@casa>
2018-11-28 19:38:46 +0100

    creo agenda con los atibutos que se especifican

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.OperationNotSupportedException;
+/**
+ *
+ * @author Manuel
+ */
+public class Agenda {
+    static final int MAX_CONTACTOS=5;
+    private int numContactos;
+}
